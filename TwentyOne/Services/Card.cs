7370d4498ae6479a32c12c5193cc68235773cd33
Christian S Snook <snookerc2208@hotmail.com>
2022-06-09 21:16:23 -0500

    Before implementing dealer hand.

--- TwentyOne/Services/Card.cs
+++ TwentyOne/Services/Card.cs
@@ -5,6 +5,7 @@
         public const int MaxSuitNumber = 4;
         public const int MaxCardNumber = 13;
 
+        public bool IsVisible { get; set; } 
         public int SuitNumber { get; set; } 
         public int CardNumber { get; set; } 
     }
