9c1c586da35982e98536603ade14eb48a3feb584
Christian S Snook <snookerc2208@hotmail.com>
2022-06-12 18:20:30 -0500

    Changed to Web Audio API.

--- TwentyOne/wwwroot/js/PlaySound.js
+++ TwentyOne/wwwroot/js/PlaySound.js
@@ -1,4 +1,15 @@
 ﻿
-window.PlayAudio = (elementName) => {
-    document.getElementById(elementName).play();
-}
+window.PlaySound = (() => {
+    let context = null;
+    return async url => {
+        if (context) context.close();
+        context = new AudioContext();
+        const source = context.createBufferSource();
+        source.buffer = await fetch(url)
+            .then(res => res.arrayBuffer())
+            .then(arrayBuffer => context.decodeAudioData(arrayBuffer));
+        source.connect(context.destination);
+        source.start();
+    };
+})();
+
