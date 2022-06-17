fd4f9623b545ef4428e03a8bf2955b07f31a1387
Christian S Snook <snookerc2208@hotmail.com>
2022-06-16 22:31:11 -0500

    Generating card numbers

--- TwentyOneTests/TwentyOneGameServiceTest.cs
+++ TwentyOneTests/TwentyOneGameServiceTest.cs
@@ -9,12 +9,10 @@ namespace TwentyOneTests
         [Fact]
         public void GivenADealerHandWithTwoCards_When16Points_ThenDealerHits()
         {
-            Hand fakeDealerHand = AutoFaker.Generate<Hand>();
-            Card card1 = new Card(1, 8);
-            Card card2 = new Card(1, 8);
+            var cardFaker = new AutoFaker<Card>()
+                .RuleFor(p => p.CardNumber, 8);
 
-            fakeDealerHand.Cards.Add(card1);
-            fakeDealerHand.Cards.Add(card2);
+            Hand fakeDealerHand = AutoFaker.Generate<Hand>();
 
             fakeDealerHand = _gameService.ComputerTurn(fakeDealerHand);
             fakeDealerHand.Score.Should().BeGreaterThan(16);
@@ -23,14 +21,8 @@ namespace TwentyOneTests
         [Fact]
         public void GivenADealerHandWithTwoCards_When17Points_ThenDealerStands()
         {
-            var gameService = new TwentyOneGameService();
-
             Hand fakeDealerHand = AutoFaker.Generate<Hand>();
-            Card card1 = new Card(1, 7);
-            Card card2 = new Card(1, 10);
 
-            fakeDealerHand.Cards.Add(card1);
-            fakeDealerHand.Cards.Add(card2);
             fakeDealerHand = _gameService.ComputerTurn(fakeDealerHand);
             fakeDealerHand.Score.Should().Be(17);
         }
