42787a3b7730e48fd70dd3892164d19f8744e5ae
Christian S Snook <snookerc2208@hotmail.com>
2022-06-16 21:58:20 -0500

    Unit tests in progress.

--- TwentyOneTests/TwentyOneGameServiceTest.cs
+++ TwentyOneTests/TwentyOneGameServiceTest.cs
@@ -1,11 +1,24 @@
+
 namespace TwentyOneTests
 {
     public class TwentyOneGameServiceTest
     {
         [Fact]
-        public void GivenADealerHand_When16Points_ThenDealerHits()
+        public void GivenADealerHandWithTwoCards_When16Points_ThenDealerHits()
         {
-            
+            Hand fakeDealerHand = new Faker<Hand>()
+                    .RuleFor(p => p.Cards, f => f.PickRandom<List<Card>>());
+
+            fakeDealerHand.Score.Should().BeGreaterThan(16);
+        }       
+        
+        [Fact]
+        public void GivenADealerHandWithTwoCards_When17Points_ThenDealerStands()
+        {
+            Hand fakeDealerHand = new Faker<Hand>()
+                    .RuleFor(p => p.Cards, f => f.PickRandom<List<Card>>());
+
+            fakeDealerHand.Score.Should().Be(17);
         }
     }
 }
--- TwentyOneTests/Usings.cs
+++ TwentyOneTests/Usings.cs
@@ -1,2 +1,8 @@
 global using Xunit;
 global using AutoBogus;
+global using FluentAssertions;
+global using FluentAssertions.Collections;
+global using FluentAssertions.Extensions;
+global using Bogus;
+global using TwentyOne.Services;
+global using TwentyOne.Models;
